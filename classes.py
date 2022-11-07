e2fe1226d5ea02d55b7a00e604649d820fbbae44
Kathan Shah <kathanshah@Kathans-MacBook-Pro.local>
2022-11-07 01:30:50 -0800

    working monte carlo estimator, but it's slightly off

--- classes.py
+++ classes.py
@@ -1,4 +1,7 @@
 import itertools
+import pickle
+import os
+
 
 class Card:
     card_values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
@@ -12,22 +15,22 @@ class Card:
 
     def __eq__(self, other):
         return self.value == other.value and self.suit == other.suit
-    
+
     def __repr__(self):
         return str([self.value, self.suit])
-    
+
     def value_for_sorting(self):
         # Number card (2-10)
         val = self.value
-        if self.value not in get_all_face_values():
+        if self.value not in Card.face_values:
             return int(val)
 
         # Face card (A, Q, J, K)
         else:
             if val == "J":
-                return 11 
+                return 11
             elif val == "Q":
-                return 12 
+                return 12
             elif val == "K":
                 return 13
             elif val == "A":
@@ -35,6 +38,7 @@ class Card:
 
             else:
                 raise ValueError
+
 
 class Hand:
     def __init__(self, card1, card2):
@@ -50,6 +54,7 @@ class Hand:
     def __eq__(self, other):
         return self.card1 == other.card1 and self.card2 == other.card2
 
+
 class Opponents:
     def __init__(self, cards_sampled, num_opponents):
         simulated_opponents_hands = dict()
@@ -64,8 +69,9 @@ class Opponents:
     def __repr__(self):
         return str(self.opp_hands)
 
-# TODO serialize this somewhere
+
 ranking_map = dict()
+ranking_map[None] = -float("inf")
 ranking_map["high_card"] = 1
 ranking_map["pair"] = 2
 ranking_map["two_pair"] = 3
@@ -76,10 +82,12 @@ ranking_map["full_house"] = 7
 ranking_map["four_of_a_kind"] = 8
 ranking_map["straight_flush"] = 9
 ranking_map["royal_flush"] = 10
-ranking_map[None] = -float("inf")
 
+with open("serialized/ranking.map", 'wb') as pickle_file:
+    pickle.dump(ranking_map, pickle_file)
 
 top_card_map = dict()
+top_card_map[None] = -float("inf")
 top_card_map["2"] = 2
 top_card_map["3"] = 3
 top_card_map["4"] = 4
@@ -93,54 +101,47 @@ top_card_map["J"] = 11
 top_card_map["Q"] = 12
 top_card_map["K"] = 13
 top_card_map["A"] = 14
-top_card_map[None] = -float("inf")
 
-# todo add functionality for checking the kickers also
-# todo might need to add more instance vars
+with open("serialized/top_card.map", 'wb') as pickle_file:
+    pickle.dump(top_card_map, pickle_file)
+
+
 class RankObject:
-    def __init__(self, ranking, top_card, kicker1=None, kicker2=None, kicker3=None):
-        self.ranking = ranking
-        self.top_card = top_card
-        self.kicker1 = kicker1
-        self.kicker2 = kicker2
-        self.kicker3 = kicker3
-
-    def __gt__(self, other):
-        if ranking_map[self.ranking] > ranking_map[other.value_for_sorting]:
-            return top_card_map[self.top_card] > top_card_map[other.top_card]
-        return False
-
-    def __lt__(self, other):
-        if ranking_map[self.ranking] < ranking_map[other.value_for_sorting]:
-            return top_card_map[self.top_card] < top_card_map[other.top_card]
-        return False
+    def __init__(self, ranking=None, top_card=None, kicker1=None, kicker2=None, kicker3=None, kicker4=None):
+        self.ranking = ranking_map[ranking]
+        self.top_card = top_card_map[top_card]
+        self.kicker1 = top_card_map[kicker1]
+        self.kicker2 = top_card_map[kicker2]
+        self.kicker3 = top_card_map[kicker3]
+        self.kicker4 = top_card_map[kicker4]
 
     def __ge__(self, other):
-        if ranking_map[self.ranking] >= ranking_map[other.value_for_sorting]:
-            return top_card_map[self.top_card] >= top_card_map[other.top_card]
-        return False
+        if self.ranking > other.ranking:
+            return True
+        elif self.ranking == other.ranking:
+            if self.top_card > other.top_card:
+                return True
+            elif self.top_card == other.top_card:
+                if self.kicker1 > other.kicker1:
+                    return True
+                elif self.kicker1 == other.kicker1:
+                    if self.kicker2 > other.kicker2:
+                        return True
+                    elif self.kicker2 == other.kicker2:
+                        if self.kicker3 > other.kicker3:
+                            return True
+                        elif self.kicker3 == other.kicker3:
+                            if self.kicker4 == other.kicker4:
+                                return True
 
-    def __le__(self, other):
-        if ranking_map[self.ranking] <= ranking_map[other.value_for_sorting]:
-            return top_card_map[self.top_card] <= top_card_map[other.top_card]
         return False
 
     def __eq__(self, other):
-        return ranking_map[self.ranking] == ranking_map[other.value_for_sorting] \
-               and top_card_map[self.top_card] == top_card_map[other.top_card]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if self.ranking == other.ranking:
+            if self.top_card == other.top_card:
+                if self.kicker1 == other.kicker1:
+                    if self.kicker2 == other.kicker2:
+                        if self.kicker3 == other.kicker3:
+                            if self.kicker4 == other.kicker4:
+                                return True
+        return False
--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
-from classes import Card, Hand, Opponents
-import itertools
+from classes import Card, Hand, RankObject, Opponents
+import itertools, pickle, random
 
 def get_all_cards():
     raw_cards = list(itertools.product(Card.card_values, Card.suits))
@@ -53,7 +53,7 @@ def royal_flush_value(cards):
         if card.value in valid_values:
             values_per_suit[card.suit].append(card.value)
         if len(values_per_suit[card.suit]) == 5:
-            return RankObject("royal_flush", "10")
+            return RankObject("royal_flush", "A")
 
     return RankObject(None, None)
 
@@ -63,37 +63,289 @@ def isStraight(cards):
     cards_values.sort()
     return cards_values == list(range(min(cards_values), min(cards_values) + 5))
 
-def get_top_card_from_five_cards(cards):
+top_card_map = pickle.load(open("serialized/top_card.map", "rb"))
+
+def get_top_card_value(cards):
     top_card_num_value = -float('inf')
     curr_top_card = None
 
     for card in cards:
         if top_card_map[card.value] > top_card_num_value:
             curr_top_card = card
-            top_card_num_value = card.value
+            top_card_num_value = top_card_map[card.value]
 
-    return card.value
+    return curr_top_card.value
 
-def hasSameSuits(cards):
+def isFlush(cards):
+    assert len(cards) == 5
     cards_suits = [c.suit for c in cards]
-    return all([c.suit == cards_suits[0].suit for c in cards])
+    return all([suit == cards_suits[0] for suit in cards_suits])
 
-def straight_flush_value(cards):
+# todo test this function
+def straight_value(cards, check_if_straight_flush=False):
     cards.sort(key=lambda c: c.value)
     cards.sort(key=lambda c: c.suit)
 
+    output = RankObject(None, None)
+    curr_top = -float("inf")
+
     for i in range(len(cards) - 5):
-        five_cards = cards[i:i+5]
+        five_cards = cards[i:i + 5]
         if isStraight(five_cards):
-            if hasSameSuits(five_cards):
-                top_card = get_top_card_from_five_cards(five_cards)
-                return RankObject("straight_flush", top_card)
+            # this if statement logic might be a little complex but it checks out on a truth table
+            if not (check_if_straight_flush and not isFlush(five_cards)):
+                top_card = get_top_card_value(five_cards)
+                if top_card_map[top_card] > curr_top:
+                    output = RankObject("straight_flush", top_card)
+                    curr_top = top_card_map[top_card]
 
-    return RankObject(None, None)
+    return output
+def straight_flush_value(cards):
+    return straight_value(cards, check_if_straight_flush=True)
 
+# todo test this function
 def four_of_a_kind_value(cards):
-    cards.sort(key=lambda c: c.suit)
+    output = RankObject()
+    seen = dict()
+    top_card = None
+    four_of_a_kind_found = False
 
+    # find four of a kind and its value
+    for card in cards:
+        if card.value not in seen:
+            seen[card.value] = 0
+        seen[card.value] += 1
+        if seen[card.value] == 4:
+            four_of_a_kind_found = True
+            top_card = card.value
+            break
+
+    # find best 5th card
+    best_5th_card = None
+    for card in cards:
+        if top_card_map[card.value] > top_card_map[best_5th_card]:
+            if card.value != top_card:
+                best_5th_card = card.value
+
+    if four_of_a_kind_found and top_card:
+        output = RankObject("four_of_a_kind", top_card, best_5th_card)
+    return output
+
+# todo test this function
+def full_house_value(cards):
+    output = RankObject()
+
+    # find the best trips
+    trips = dict()
+    for card in cards:
+        if card.value not in trips:
+            trips[card.value] = 0
+        trips[card.value] += 1
+
+    top_trips_value = None
+    for card_value_seen in trips:
+        if trips[card_value_seen] == 3:
+            if top_card_map[card_value_seen] > top_card_map[top_trips_value]:
+                top_trips_value = card_value_seen
+
+    # find the best pair
+    pairs = dict()
+    for card in cards:
+        if card.value not in pairs:
+            pairs[card.value] = 0
+        pairs[card.value] += 1
+
+    top_pair_value = None
+    for card_value_seen in pairs:
+        if card_value_seen != top_trips_value and pairs[card_value_seen] >= 2:
+            if top_card_map[card_value_seen] > top_card_map[top_pair_value]:
+                top_pair_value = card_value_seen
+
+    if top_trips_value and top_pair_value:
+        output = RankObject("full_house", top_trips_value, top_pair_value)
+
+    return output
+
+def flush_value(cards):
+    output = RankObject()
+
+    # get dict to sort cards by suit
+    suits = dict()
+    for suit in Card.suits:
+        suits[suit] = []
+
+    # add each card to the appropriate suit
+    for card in cards:
+        suits[card.suit].append(card)
+
+    # check if any has a suit
+    for suit in suits:
+        cards_with_given_suit = suits[suit]
+        if len(cards_with_given_suit) >= 5:
+            # found our flush
+            top_card = get_top_card_value(cards_with_given_suit)
+            output = RankObject("flush", top_card)
+            break
+
+    return output
+
+def three_of_a_kind_value(cards):
+    # todo i copied this from the fullhouse function, there might be a better way of
+    # todo abstracting it so i dont have to copy code
+    output = RankObject()
+
+    # find the best trips
+    trips = dict()
+    for card in cards:
+        if card.value not in trips:
+            trips[card.value] = 0
+        trips[card.value] += 1
+
+    top_trips_value = None
+    for card_value_seen in trips:
+        if trips[card_value_seen] == 3:
+            if top_card_map[card_value_seen] > top_card_map[top_trips_value]:
+                top_trips_value = card_value_seen
+
+    # find kicker 1
+    cards_to_check = []
+    for card in cards:
+        if card.value != top_trips_value:
+            cards_to_check.append(card)
+    kicker1 = get_top_card_value(cards_to_check)
+
+    # find kicker 2
+    cards_to_check = []
+    for card in cards:
+        if card.value != top_trips_value and card.value != kicker1:
+            cards_to_check.append(card)
+    kicker2 = get_top_card_value(cards_to_check)
+
+    if top_trips_value:
+        output = RankObject("three_of_a_kind", top_trips_value, kicker1, kicker2)
+
+    return output
+
+def two_pair_value(cards):
+    # todo i copied this from the fullhouse function, there might be a better way of
+    # todo abstracting it so i dont have to copy code
+
+    output = RankObject()
+
+    # find the best pairs1
+    pairs1 = dict()
+    for card in cards:
+        if card.value not in pairs1:
+            pairs1[card.value] = 0
+        pairs1[card.value] += 1
+
+    top_pair1_value = None
+    for card_value_seen in pairs1:
+        if pairs1[card_value_seen] == 2:
+            if top_card_map[card_value_seen] > top_card_map[top_pair1_value]:
+                top_pair1_value = card_value_seen
+
+    # find the best pairs2
+    pairs2 = dict()
+    for card in cards:
+        if card.value not in pairs2:
+            pairs2[card.value] = 0
+        pairs2[card.value] += 1
+
+    top_pair2_value = None
+    for card_value_seen in pairs2:
+        if card_value_seen != top_pair1_value and pairs2[card_value_seen] == 2:
+            if top_card_map[card_value_seen] > top_card_map[top_pair2_value]:
+                top_pair2_value = card_value_seen
+
+    # find kicker 1
+    cards_to_check = []
+    for card in cards:
+        if card.value != top_pair1_value and card.value != top_pair2_value:
+            cards_to_check.append(card)
+    kicker1 = get_top_card_value(cards_to_check)
+
+    if top_pair1_value and top_pair2_value:
+        output = RankObject("two_pair", top_pair1_value, top_pair2_value, kicker1)
+
+    return output
+
+def pair_value(cards):
+    # todo i copied this from the three_of_a_kind code, try to abstract it better!!
+    output = RankObject()
+
+    # find the best pairs
+    pairs = dict()
+    for card in cards:
+        if card.value not in pairs:
+            pairs[card.value] = 0
+        pairs[card.value] += 1
+
+    top_pair_value = None
+    for card_value_seen in pairs:
+        if pairs[card_value_seen] == 2:
+            if top_card_map[card_value_seen] > top_card_map[top_pair_value]:
+                top_pair_value = card_value_seen
+
+    # find kicker 1
+    cards_to_check = []
+    for card in cards:
+        if card.value != top_pair_value:
+            cards_to_check.append(card)
+    kicker1 = get_top_card_value(cards_to_check)
+
+    # find kicker 2
+    cards_to_check = []
+    for card in cards:
+        if card.value != top_pair_value and card.value != kicker1:
+            cards_to_check.append(card)
+    kicker2 = get_top_card_value(cards_to_check)
+
+    # find kicker 3
+    cards_to_check = []
+    for card in cards:
+        if card.value != top_pair_value and card.value != kicker1 and card.value != kicker2:
+            cards_to_check.append(card)
+    kicker3 = get_top_card_value(cards_to_check)
+
+    if top_pair_value:
+        output = RankObject("pair", top_pair_value, kicker1, kicker2, kicker3)
+
+    return output
+
+def high_card_value(cards):
+    high_card_value = get_top_card_value(cards)
+
+    # find kicker 1
+    cards_to_check = []
+    for card in cards:
+        if card.value != high_card_value:
+            cards_to_check.append(card)
+    kicker1 = get_top_card_value(cards_to_check)
+
+    # find kicker 2
+    cards_to_check = []
+    for card in cards:
+        if card.value != high_card_value and card.value != kicker1:
+            cards_to_check.append(card)
+    kicker2 = get_top_card_value(cards_to_check)
+
+    # find kicker 3
+    cards_to_check = []
+    for card in cards:
+        if card.value != high_card_value and card.value != kicker1 and card.value != kicker2:
+            cards_to_check.append(card)
+    kicker3 = get_top_card_value(cards_to_check)
+
+    # find kicker 4
+    cards_to_check = []
+    for card in cards:
+        if card.value != high_card_value and card.value != kicker1 and card.value != kicker2 \
+                and card.value != kicker3:
+            cards_to_check.append(card)
+    kicker4 = get_top_card_value(cards_to_check)
+
+    return RankObject("high_card", high_card_value, kicker1, kicker2, kicker3, kicker4)
 
 def get_hand_ranking(hand, board):
     cards_from_hand = [hand.card1, hand.card2]
@@ -101,7 +353,7 @@ def get_hand_ranking(hand, board):
 
     assert len(seven_cards) == 7, "there must be 2 cards in the hand and 5 on the board"
 
-    does_not_exist = RankObject(None, None)
+    does_not_exist = RankObject()
 
     if royal_flush_value(seven_cards) != does_not_exist:
         return royal_flush_value(seven_cards)
@@ -109,30 +361,61 @@ def get_hand_ranking(hand, board):
         return straight_flush_value(seven_cards)
     elif four_of_a_kind_value(seven_cards) != does_not_exist:
         return four_of_a_kind_value(seven_cards)
-    elif is_full_house(seven_cards):
-        return FullHouse(seven_cards)
-    elif is_flush(seven_cards):
-        return Flush(seven_cards)
-    elif is_straight(seven_cards):
-        return Straight(seven_cards)
-    elif is_three_of_a_kind(seven_cards):
-        return ThreeOfAKind(seven_cards)
-    elif is_two_pair(seven_cards):
-        return TwoPair(seven_cards)
-    elif is_pair(seven_cards):
-        return Pair(seven_cards)
+    elif full_house_value(seven_cards) != does_not_exist:
+        return full_house_value(seven_cards)
+    elif flush_value(seven_cards) != does_not_exist:
+        return flush_value(seven_cards)
+    elif straight_value(seven_cards) != does_not_exist:
+        return straight_value(seven_cards)
+    elif three_of_a_kind_value(seven_cards) != does_not_exist:
+        return three_of_a_kind_value(seven_cards)
+    elif two_pair_value(seven_cards) != does_not_exist:
+        return two_pair_value(seven_cards)
+    elif pair_value(seven_cards) != does_not_exist:
+        return pair_value(seven_cards)
     else:
-        return HighCard(seven_cards)
+        return high_card_value(seven_cards)
 
 def my_hand_is_better(my_hand, opp_hand, board):
     my_hand_ranking = get_hand_ranking(my_hand, board)
     opp_hand_ranking = get_hand_ranking(opp_hand, board)
 
-    if my_win_condition >= opp_win_condition: # ties are counted as wins
+    if my_hand_ranking >= opp_hand_ranking: # ties are counted as wins
         return True
     else:
         return False
 
+def monte_carlo_winning_chance_estimator(hand, num_opponents, cards_on_board, iterations=1000):
+    assert type(hand) is Hand
+    assert num_opponents >= 1 and num_opponents <= 10
+    assert len(cards_on_board) <= 5
 
+    valid_cards = get_remaining_cards(hand, cards_on_board)
 
+    wins = 0
+    for _ in range(iterations):
+        num_cards_from_opponents = num_opponents * 2
+        num_cards_left_to_reveal = 5 - len(cards_on_board)
+        num_of_cards_to_sample = num_cards_left_to_reveal + num_cards_from_opponents
 
+        cards_sampled = random.sample(valid_cards, num_of_cards_to_sample)
+
+        simulated_opps = get_simulated_opponents_hands(cards_sampled, num_opponents)
+        simulated_remaining_cards_on_board = get_simulated_cards_on_board(cards_sampled, num_cards_left_to_reveal)
+
+        print("OPPS", simulated_opps)
+        print("REMAINING CARDS ON BOARD", simulated_remaining_cards_on_board)
+
+        board = cards_on_board + simulated_remaining_cards_on_board
+
+        win = 1
+        for opponent_num in simulated_opps.opp_hands:
+            opp_hand = simulated_opps.opp_hands[opponent_num]
+            if not my_hand_is_better(hand, opp_hand, board):
+                print("lost")
+                win = 0
+                break
+
+        wins += win
+
+    return wins / iterations
